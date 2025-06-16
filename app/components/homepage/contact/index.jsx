66969efca9251392b3573e72ca1aaaac73d28119
Mouhamed OUMAR <mohamedmohamedoumaramar@gmail.com>
2025-06-16 17:37:52 +0200

    I change some information on my cv

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -22,30 +22,26 @@ function ContactSection() {
         <ContactForm />
         <div className="lg:w-3/4 ">
           <div className="flex flex-col gap-5 lg:gap-9">
-            <p className="text-sm md:text-xl flex items-center gap-3">
+            <Link href={personalData.email.link} className="text-sm md:text-xl flex items-center gap-3 hover:text-[#16f2b3] transition-colors duration-300">
               <MdAlternateEmail
                 className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
                 size={36}
               />
-              <span>{personalData.email}</span>
-            </p>
-            <p className="text-sm md:text-xl flex items-center gap-3">
+              <span>{personalData.email.address}</span>
+            </Link>
+            <Link href={personalData.phone.link} className="text-sm md:text-xl flex items-center gap-3 hover:text-[#16f2b3] transition-colors duration-300">
               <IoMdCall
                 className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
                 size={36}
               />
-              <span>
-                {personalData.phone}
-              </span>
-            </p>
+              <span>{personalData.phone.number}</span>
+            </Link>
             <p className="text-sm md:text-xl flex items-center gap-3">
               <CiLocationOn
                 className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
                 size={36}
               />
-              <span>
-                {personalData.address}
-              </span>
+              <span>{personalData.address}</span>
             </p>
           </div>
           <div className="mt-8 lg:mt-16 flex items-center gap-5 lg:gap-10">
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,8 +5,14 @@ export const personalData = {
   description: `Detail-oriented Computer Science student with hands-on experience in Java, Spring Boot, and Angular. 
   Skilled in REST API development, object-oriented programming, and network troubleshooting (CCNA, GSM).
   Passionate about solving technical challenges and building scalable backend systems.`,
-  email: "mohamedmohamedoumaramar@gmail.com",
-  phone: "+222 46475363",
+  email: {
+    address: "mohamedmohamedoumaramar@gmail.com",
+    link: "mailto:mohamedmohamedoumaramar@gmail.com"
+  },
+  phone: {
+    number: "+222 46475363",
+    link: "tel:+22246475363"
+  },
   address: "Nouakchott, Mauritania",
   github: "https://github.com/MouhamedOUMAR",
   facebook: "https://www.facebook.com/share/16M89GPeqP/?mibextid=wwXIfr",
